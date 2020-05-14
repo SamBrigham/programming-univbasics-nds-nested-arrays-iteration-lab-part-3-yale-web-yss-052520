6175dd4eaead5ce95f2d37de5962a3bcefc6e4be
Samuel Brigha <sam.brigham@yale.edu>
2020-05-14 20:24:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,9 +5,9 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do 
     if src[row_index][element_index].is_a?(String)
-      message << src[row_index][element_index]
-      element_index += 1
+      message += src[row_index][element_index]
     end
+      element_index += 1
   end
   row_index += 1
 end
